b7e848c6ff34b30d31e8db03ee1177229c788599
flyingpig233-cpu <1686079252@qq.com>
2022-08-15 17:50:34 +0800

    # Title: Summary, imperative, start upper case, don't end with a period # No more than 50 chars. #### 50 chars is here:  # Preliminary realization of running code function and configuration function # Remember blank line between title and body.
    # Body: Explain *what* and *why* (not *how*). Include task ID (Jira issue).
    # Wrap at 72 chars. ################################## which is here:  #
    Using clap to manage the command lines and now can use oj_tools run <filename> to run the code.
    
    # At the end: Include Co-authored-by for all contributors.
    # Include at least one empty line before it. Format:
    # Co-authored-by: name <user@users.noreply.github.com>
    #
    # How to Write a Git Commit Message:
    # https://chris.beams.io/posts/git-commit/
    #
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how

---
+++ src/oj_tools.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
+use config::Config;
+use run_script::ScriptOptions;
+
+use crate::{Action, HelperCli};
+
+pub enum OJType {
+    Luogu,
+    Codeforces,
+}
+
+pub struct OJTools {
+    args: HelperCli,
+    oj_type: OJType,
+    config: HashMap<String, String>,
+}
+
+impl OJTools {
+    pub fn new(args: HelperCli, oj_type: OJType, config_path: Option<String>) -> Self {
+        let config = Config::builder()
+            .add_source(config::File::with_name(
+                dirs::home_dir().unwrap()
+                    .join(PathBuf::from(".oj_tools/config.toml")).to_str().unwrap()))
+            .build()
+            .unwrap()
+            .try_deserialize::<HashMap<String, String>>()
+            .unwrap();
+        Self {
+            args,
+            oj_type,
+            config,
+        }
+    }
+
+    pub fn run(&self) {
+        println!("{:?}", self.config);
+        match &self.args.action {
+            Action::Test => { self.run_test() }
+            Action::Run { filename, .. } => {
+                self.run_code(Path::new(filename),
+                              Path::new(self.config.get("script_path")
+                                  .unwrap()
+                                  .replace("~", dirs::home_dir().unwrap().to_str().unwrap())
+                                  .as_str()));
+            }
+
+            Action::Generate => { self.gen_code() }
+            _ => {}
+        }
+    }
+
+    fn run_test(&self) {}
+
+    fn run_code(&self, filepath: &Path, script_path: &Path) {
+        let mut template = String::new();
+        println!("{:?}", script_path);
+        fs::File::open(script_path).unwrap().read_to_string(&mut template).unwrap();
+        let content = template
+            .replace("$fileNameWithoutExt$", filepath.file_stem().unwrap().to_str().unwrap())
+            .replace("$fileName$", filepath.to_str().unwrap())
+            .replace("$dir$", filepath.parent().unwrap().to_str().unwrap());
+        println!("{}", content);
+        let options = ScriptOptions::new();
+        let args = vec![];
+
+        println!("Running script...");
+        let (code, output, error) = run_script::run(
+            &*content,
+            &args,
+            &options,
+        ).unwrap();
+        println!("{}", output);
+        println!("Exit Code: {}", code);
+
+        // run part
+    }
+
+    fn gen_code(&self) {}
+}
